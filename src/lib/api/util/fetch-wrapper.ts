b42b724b3d48b47ecdccb76b5d24f901743b740a
Marvin Yang <marvinyangs@gmail.com>
2025-02-19 17:17:01 -0500

    feat: navigate to login page if unauthorized

--- src/lib/api/util/fetch-wrapper.ts
+++ src/lib/api/util/fetch-wrapper.ts
@@ -1,3 +1,6 @@
+import { goto } from '$app/navigation';
+import { base } from '$app/paths';
+
 export async function fetchWrapper(url: string, options: RequestInit = {}) {
 	const response = await fetch(url, {
 		headers: {
@@ -7,6 +10,9 @@ export async function fetchWrapper(url: string, options: RequestInit = {}) {
 		...options
 	});
 	if (!response.ok) {
+		if (response.status === 401) {
+			goto(`${base}/login`);
+		}
 		throw new Error(`API error: ${response.statusText}`);
 	}
 	return response;
